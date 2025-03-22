d85c8f31757c03488b20a7fd35a6b3a9c630ac47
Leandro Palmeira <leandropalmeira@hotmail.com>
2025-03-22 00:55:36 -0300

    Update _backend.sh

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -148,7 +148,7 @@ backend_update() {
   pm2 stop ${empresa_atualizar}-backend
   git pull
   cd /home/deploy/${empresa_atualizar}/backend
-  npm install --force
+  npm install --legacy-peer-deps
   npm update -f
   npm install @types/fs-extra
   rm -rf dist 
